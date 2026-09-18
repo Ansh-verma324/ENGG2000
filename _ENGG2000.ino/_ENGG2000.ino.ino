e8ed14a592c85cf32d6abc85a76b383da5551a6d
Ansh-verma324 <ansh.verma@students.mq.edu.au>
2026-09-18 15:16:53 +1000

    Added gyroscope code and encoders

--- _ENGG2000.ino/_ENGG2000.ino.ino
+++ _ENGG2000.ino/_ENGG2000.ino.ino
@@ -8,18 +8,38 @@
 * EN = D3, PH = D11, Laser = D7, IR Sensor = D8
 */
 
+#include <Wire.h>
+#include <MPU6050_light.h>
+
+MPU6050 mpu(Wire);
+
 // ============================================
 // PIN DEFINITIONS
 // ============================================
-const int irPin = 8; // IR sensor on D8
-const int laserPin = 7; // Laser on D7
 const int enPin = 3; // Motor speed (PWM) → DRV8874 EN/IN1
 const int phPin = 11; // Motor direction → DRV8874 PH/IN2
+
+const int encA = 6; // DRV8874 EncoderA
+const int encB = 5; // DRV8874 EnocderB
+
+const int laserPin = 7; // Laser on D7
+
+const int irPin = 8; // IR sensor on D8
+// ============================================
+// GYRO GLOBAL VARIABLES
+// ============================================
+float gyroX;
+float gyroY;
+float gyroZ;
 
 // ============================================
 // SETUP
 // ============================================
 void setup() {
+  Wire.begin();
+
+  mpu.begin();
+  mpu.calcOffsets();
 
   pinMode(irPin, INPUT_PULLUP);
   pinMode(laserPin, OUTPUT);
@@ -32,6 +52,11 @@ void setup() {
 // MAIN LOOP
 // ============================================
 void loop() {
+
+  mpu.update();
+  gyroX = mpu.getGyroX();
+  gyroY = mpu.getGyroY();
+  gyroZ = mpu.getGyroZ();
 
   digitalWrite(phPin, HIGH);
   analogWrite(enPin, 65);
@@ -50,6 +75,6 @@ void loop() {
     digitalWrite(phPin, HIGH);
     analogWrite(enPin, 65);
     digitalWrite(laserPin, LOW);
-    delay(50); // Ignore sensor during motor startup spike
+    delay(50);
   }
 }
